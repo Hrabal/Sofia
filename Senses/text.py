611a7e98c5ed52a0d1f181742fa225efcb950e0a
Federico Cerchiari <federicocerchiari@gmail.com>
2015-04-14 12:37:52 +0200

    Create text.py

---
+++ Senses/text.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Text/Strings sense/module for Sofia
+'''
+
+from collections import deque
+
+class VerbalMemoryNode(object):
+	__slots__ = ['value','children']
+
+	def __init__(self, value):
+		self.value = value
+		self.children = {}
+
+class VerbalMemory(object):
+	def __init__(self):
+		self.root = VerbalMemoryNode(None)
+
+	def add_word(self, s):
+		current = self.root
+		for c in s:
+			if c in current.children:
+				current = current.children[c]
+			else:
+				new_node = VerbalMemoryNode(c)
+				current.children[c] = new_node
+				current = new_node
+		if None not in current.children:
+			current.children[None] = None
+
+	def contains(self, s):
+		current = self.root
+		for c in s:
+			if c in current.children:
+				current = current.children[c]
+			else:
+				return False
+		return None in current.children
+
+	def starts_with(self, prefix):
+		current = self.root
+		for c in prefix:
+			if c in current.children:
+				current = current.children[c]
+			else:
+				return []
+		results = []
+		nodes = deque([(current, prefix)])
+		while nodes:
+			node, prefix = nodes.pop()
+			for key, child in node.children.items():
+				if key is None:
+					results.append(prefix)
+				else:
+					nodes.append((child, prefix + key))
+		return results
